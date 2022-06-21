1b57344045fbe223341d2d7f247e00d2ebf10e8f
luiizsps <lsps1019@gmail.com>
2022-06-21 00:50:36 -0300

    multiple linear regression

---
+++ MultipleLinearRegression.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("FuelConsumptionCo2.csv")
+df = df[['CO2EMISSIONS', 'ENGINESIZE', 'CYLINDERS', 'FUELCONSUMPTION_COMB']]
+
+adf = np.random.rand(len(df)) < 0.8
+train = df[adf]
+test = df[~adf]
+
+train_x = np.asanyarray(df[['ENGINESIZE', 'CYLINDERS', 'FUELCONSUMPTION_COMB']])
+train_y = np.asanyarray(df[['CO2EMISSIONS']])
+
+from sklearn import linear_model
+
+regre = linear_model.LinearRegression()
+regre.fit(train_x, train_y)
+
+print('Coeficientes:', regre.coef_)
+print('Intersecção', regre.intercept_)
+
+test_x = np.asanyarray(test[['ENGINESIZE', 'CYLINDERS', 'FUELCONSUMPTION_COMB']])
+test_y = np.asanyarray(test[['CO2EMISSIONS']])
+
+print('Score: ', regre.score(test_x, test_y))
--- linear-regression2.py
+++ linear-regression2.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import sklearn.linear_model as lm
+from sklearn.metrics import r2_score
 
 df = pd.read_csv("FuelConsumptionCo2.csv")
 cdf = df[["CYLINDERS", "CO2EMISSIONS"]]
@@ -25,12 +26,10 @@ print('Equação da reta: ',coeficient[0][0],'x + ',interception[0])
 plt.scatter(df.CYLINDERS, df.CO2EMISSIONS, color="blue")
 
 # plota reta vermelha no grafico através da previvão de resultados 
-plt.plot(train_x, coeficient[0][0]*train_x + interception[0], 'r-')
+plt.plot(train_x, coeficient[0][0]*train_x + interception[0], 'k-')
 plt.xlabel("Number of Cylinders")
 plt.ylabel("Co2 Emission")
 plt.show()
-
-from sklearn.metrics import r2_score
 
 test_x = np.asanyarray(test[['CYLINDERS']])
 test_y = np.asanyarray(test[['CO2EMISSIONS']])
@@ -41,5 +40,5 @@ test_y_ = regression.predict(test_x)
 # Calcula erro médio absoluto, soma dos quadrados residuais e score  
 print("Mean absolute error: %.2f" % np.mean(np.absolute(test_y_ - test_y)))
 print("Residual sum of squares (MSE): %.2f" % np.mean((test_y_ - test_y) ** 2))
-print("R2-score: %.2f" % r2_score(test_y , test_y_))
+print("Relative squared error: %.2f" % r2_score(test_y , test_y_))
 
