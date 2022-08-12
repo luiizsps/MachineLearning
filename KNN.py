45d90a4886338cae96a546e71fd8e5398594407d
luiizsps <103710492+luiizsps@users.noreply.github.com>
2022-08-12 19:11:09 -0300

    Add files via upload

---
+++ KNN.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn import preprocessing
+
+df = pd.read_csv('teleCust1000t.csv')
+df.head()
+
+df['custcat'].value_counts()
+df.hist(column='income', bins=50)
+df.columns
+
+X = df[['region', 'tenure','age', 'marital', 'address', 'income', 'ed', 'employ','retire', 'gender', 'reside']] .values  #.astype(float)
+X[0:5]
+y = df['custcat'].values
+y[0:5]
+X = preprocessing.StandardScaler().fit(X).transform(X.astype(float))
+X[0:5]
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.2, random_state=4)
+print ('Train set:', X_train.shape,  y_train.shape)
+print ('Test set:', X_test.shape,  y_test.shape)
+
+from sklearn.neighbors import KNeighborsClassifier
+
+k = 2
+#Train Model and Predict  
+neigh = KNeighborsClassifier(n_neighbors = k).fit(X_train,y_train)
+yhat = neigh.predict(X_test)
+yhat[0:5]
+
+from sklearn import metrics
+print("Train set Accuracy: ", metrics.accuracy_score(y_train, neigh.predict(X_train)))
+print("Test set Accuracy: ", metrics.accuracy_score(y_test, yhat))
