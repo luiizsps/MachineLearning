5042215fcbbde614c4110c224b18a762ce3bf81c
Luiz Passos <lsps1019@gmail.com>
2023-03-14 22:15:25 -0300

    Create mnist_tensorflow_lite.py

---
+++ tensorflow_lite/mnist_tensorflow_lite.py
@@ -0,0 +1,82 @@
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import os
+
+MODELS_DIR = 'models/'
+if not os.path.exists(MODELS_DIR):
+  os.mkdir(MODELS_DIR)
+MODEL_TF = MODELS_DIR + 'model'
+MODEL_NO_QUANT_TFLITE = MODELS_DIR + 'model_no_quant.tflite'
+MODEL_TFLITE = MODELS_DIR + 'model.tflite'
+MODEL_TFLITE_MICRO = MODELS_DIR + 'model.cc'
+
+
+(ds_train, ds_test), ds_info = tfds.load(
+    'mnist',
+    split=['train', 'test'],
+    shuffle_files=True,
+    as_supervised=True,
+    with_info=True,
+)
+
+def normalize_img(image, label):
+  """Normalizes images: `uint8` -> `float32`."""
+  return tf.cast(image, tf.float32) / 255., label
+
+ds_train = ds_train.map(
+    normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
+ds_train = ds_train.cache()
+ds_train = ds_train.shuffle(ds_info.splits['train'].num_examples)
+ds_train = ds_train.batch(128)
+ds_train = ds_train.prefetch(tf.data.AUTOTUNE)
+
+ds_test = ds_test.map(
+    normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
+ds_test = ds_test.batch(128)
+ds_test = ds_test.cache()
+ds_test = ds_test.prefetch(tf.data.AUTOTUNE)
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dense(10)
+])
+
+model.compile(optimizer='adam', loss='mse', metrics=['mae'])
+
+history = model.fit(
+    ds_train,
+    epochs=300,
+    validation_data=ds_test,
+)
+model.save(MODEL_TF)
+
+train_mae = history.history['mae']
+val_mae = history.history['val_mae']
+epochs = range(1,301)
+
+plt.plot(epochs, train_mae, 'g.', label='Training MAE')
+plt.plot(epochs, val_mae, 'b.', label='Validation MAE')
+plt.title('Training and validation mean absolute error')
+plt.xlabel('Epochs')
+plt.ylabel('MAE')
+plt.legend()
+plt.show()
+
+converter = tf.lite.TFLiteConverter.from_saved_model(MODEL_TF)
+model_no_quant_tflite = converter.convert()
+
+open(MODEL_NO_QUANT_TFLITE, "wb").write(model_no_quant_tflite)
+
+size_tf = os.path.getsize(MODEL_TF)
+size_no_quant_tflite = os.path.getsize(MODEL_NO_QUANT_TFLITE)
+
+size = pd.DataFrame.from_records(
+  [["TensorFlow", f"{size_tf} bytes", ""],
+   ["TesorFlow Lite", f"{size_no_quant_tflite} bytes", f"(Reduced by {size_tf - size_no_quant_tflite} bytes)"]],
+  columns = ["Model","Size",""], index = "Model")
+
+print(size)
